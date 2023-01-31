47165bfc0ab29eecf93b7d99dc391efe4075d2f0
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2023-01-31 08:56:48 -0600

    started on the other two greedy approaches

---
+++ Greedy Algorithms/greedy_value.py
@@ -0,0 +1,28 @@
+def read_file(filename):
+    data = []
+    with open(filename) as f:
+        for line in f:
+            data.append(list(map(int, line.strip().split(','))))
+    return data
+
+def knapsack(items, maxWeight):
+    items.sort(key=lambda x: x[1], reverse=True)
+    n = len(items)
+    totalValue = 0
+
+    for i in range(n):
+        if maxWeight == 0:
+            return totalValue
+        if items[i][0] <= maxWeight:
+            totalValue += items[i][1]
+            maxWeight -= items[i][0]
+        else:
+            fraction = maxWeight / items[i][0]
+            totalValue += items[i][1] * fraction
+            maxWeight = 0
+
+    return totalValue
+
+items = [(2,3),(3,4),(4,5),(5,8),(9,10)]
+maxWeight = 20
+print(knapsack(items, maxWeight))
